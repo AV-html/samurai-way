be741d533d095b13fa39e26d0cc94a4958c1ab50
Alexander Volkov <AV-html@yandex.ru>
2022-04-12 19:57:43 +0300

    07 - components

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <a href="/home">Home</a>
+            <a href="/feed">News Feed</a>
+            <a href="/messages">Messages</a>
+        </div>
+    );
+}
+
+function Technologies() {
+    return (
         <ul>
             <li>css</li>
             <li>html</li>
             <li>js</li>
             <li>react</li>
         </ul>
-    </div>
-  );
+    );
 }
 
 export default App;
