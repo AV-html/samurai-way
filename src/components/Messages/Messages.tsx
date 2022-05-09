ebbd1a97b4c0b8c5c0011a00219e2be9dc0547d4
Alexander Volkov <AV-html@yandex.ru>
2022-05-09 11:25:20 +0300

    21 - Messages layout

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -3,8 +3,40 @@ import d from './Messages.module.css'
 
 export function Messages() {
     return (
-        <div>
-            Messages
+        <div className={d.messages}>
+            <h3 className={d.title}>Messages</h3>
+            <div className={d.dialogs}>
+                <ul className={d.list}>
+                    <li className={d.item + ' ' + d.active}>
+                        <img src="https://www.montrealtherapy.com/wp-content/uploads/2016/07/avatars-4.png"
+                             alt=""/>
+                        Evgeniy
+                    </li>
+                    <li className={d.item}>
+                        <img src="https://www.montrealtherapy.com/wp-content/uploads/2016/07/avatars-4.png"
+                             alt=""/>
+                        Mark
+                    </li>
+                    <li className={d.item}>
+                        <img src="https://www.montrealtherapy.com/wp-content/uploads/2016/07/avatars-4.png"
+                             alt=""/>
+                        Aleksey
+                    </li>
+                    <li className={d.item}>
+                        <img src="https://www.montrealtherapy.com/wp-content/uploads/2016/07/avatars-4.png"
+                             alt=""/>
+                        Pavel
+                    </li>
+                </ul>
+
+                <ul className={d['message-list']}>
+                    <li className={d.message}>Hello!</li>
+                    <li className={d.message}>Hi!</li>
+                    <li className={d.message}>How are you?</li>
+                </ul>
+            </div>
+
+
         </div>
     );
 }
