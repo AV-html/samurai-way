14804f47155a7acb9f4dbfbc179cfe54cb5b4bb4
Alexander Volkov <AV-html@yandex.ru>
2022-05-13 09:50:38 +0300

    25 - ui/bll

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -5,23 +5,53 @@ import {Message} from './Message/Message';
 
 
 export function Messages() {
+
+    type DialogsDataType = Array<DialogType>
+    type DialogType = {
+        id: string
+        name: string
+    }
+
+    const dialogsData: DialogsDataType = [
+        {id: '1', name: 'Evgeniy'},
+        {id: '2', name: 'Mark'},
+        {id: '3', name: 'Aleksey'},
+        {id: '4', name: 'Pavel'},
+        {id: '5', name: 'Oksana'},
+        {id: '6', name: 'Vlad'}
+    ]
+
+    type MessagesDataType = Array<MessagesType>
+    type MessagesType = {
+        id: string
+        message: string
+    }
+
+    const messagesData: MessagesDataType = [
+        {id: '1', message: 'Hello!'},
+        {id: '2', message: 'Hi!'},
+        {id: '3', message: 'How are you'},
+        {id: '4', message: 'You\'re busy?'}
+    ]
+
     return (
         <div className={d.messages}>
             <h3 className={d.title}>Messages</h3>
             <div className={d.dialogs}>
                 <ul className={d.list}>
-                    <DialogItem name={'Evgeniy'} id={'1'}/>
-                    <DialogItem name={'Mark'} id={'2'}/>
-                    <DialogItem name={'Aleksey'} id={'3'}/>
-                    <DialogItem name={'Pavel'} id={'4'}/>
-                    <DialogItem name={'Oksana'} id={'5'}/>
+                    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                    <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                    <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                    <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                    <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
                 </ul>
 
                 <ul className={d['message-list']}>
-                    <Message message={'Hello!'}/>
-                    <Message message={'Hi!'}/>
-                    <Message message={'How are you'}/>
-                    <Message message={'You\'re busy?'}/>
+                    <Message message={messagesData[0].message}/>
+                    <Message message={messagesData[1].message}/>
+                    <Message message={messagesData[2].message}/>
+                    <Message message={messagesData[3].message}/>
                 </ul>
             </div>
         </div>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,34 @@ import p from './MyPosts.module.css';
 import {Post} from './Post/Post';
 
 export function MyPosts() {
+
+
+    type PostsDataType = Array<PostType>
+    type PostType = {
+        id: string
+        message: string
+        likesCount: number
+    }
+
+    const postsData: PostsDataType = [
+        {
+            id: '1',
+            message: '\'Doloremque dolorum, exercitationem libero nam odit porro quaerat, sequi similique tempore? Cumque, illum, optio.\'!',
+            likesCount: 15
+        },
+        {
+            id: '2',
+            message: 'Consectetur adipisicing elit. Alias distinctio eius est laborum magni rerum!',
+            likesCount: 20
+        },
+        {
+            id: '3',
+            message: 'Adipisci aut culpa doloremque dolorum, exercitationem libero nam odit porro quaerat sequ',
+            likesCount: 1
+        }
+    ]
+
+
     return (
         <>
             <div className={p['new-post']}>
@@ -17,17 +45,16 @@ export function MyPosts() {
             </div>
             <ul className={p['post-list']}>
                 <Post
-                    message={'Doloremque dolorum, exercitationem libero nam odit porro quaerat, sequi similique tempore? Cumque, illum, optio.'}
-                    likesCount={15}
+                    message={postsData[0].message}
+                    likesCount={postsData[0].likesCount}
                 />
-
-                <Post message={'Consectetur adipisicing elit. Alias distinctio eius est laborum magni rerum!'}
-                      likesCount={20}
-                />
-
                 <Post
-                    message={'Adipisci aut culpa doloremque dolorum, exercitationem libero nam odit porro quaerat sequ'}
-                    likesCount={1}
+                    message={postsData[1].message}
+                    likesCount={postsData[1].likesCount}
+                />
+                <Post
+                    message={postsData[2].message}
+                    likesCount={postsData[2].likesCount}
                 />
             </ul>
         </>
